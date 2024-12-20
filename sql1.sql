ff3fda63689e49b9ce5a1993f79aedb120d06743
Stanley_Snyder <soyfranque@gmail.com>
2024-12-20 19:27:40 +0300

    Добавили свой первый файл хых хых хы

---
+++ sql1.sql
@@ -0,0 +1,12 @@
+-- USE userdb;
+-- CREATE TABLE users (
+-- 	id INTEGER AUTO_INCREMENT PRIMARY KEY,
+-- 	firstname VARCHAR(30),
+--     age INTEGER
+-- );
+-- INSERT INTO users (firstname, age) VALUES ('Tom', 34);
+
+-- USE userdb;
+-- SELECT * FROM users;
+
+-- DROP TABLE users; 
